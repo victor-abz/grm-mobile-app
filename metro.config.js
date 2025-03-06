0d569a9d75e87a8296c36684aee24de79369b963
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2025-03-06 12:32:24 +0000

    Updated project expo 43 to expo 47 and removed 'react-native-code-input' package and used 'react-native-confirmation-code-field' package in its place

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,31 @@
+// const { getDefaultConfig } = require("@expo/metro-config");
+
+// const defaultConfig = getDefaultConfig(__dirname);
+
+// module.exports = {
+//   ...defaultConfig,
+//   transformer: {
+//     ...defaultConfig.transformer,
+//     babelTransformerPath: require.resolve("react-native-svg-transformer"),
+//   },
+//   resolver: {
+//     ...defaultConfig.resolver,
+//     assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+//     sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
+//   },
+// };
 const { getDefaultConfig } = require("@expo/metro-config");
-
 const defaultConfig = getDefaultConfig(__dirname);
-
+const assetExt = defaultConfig.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
 module.exports = {
-  ...defaultConfig,
   transformer: {
-    ...defaultConfig.transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
   resolver: {
-    ...defaultConfig.resolver,
-    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+    assetExts: [...assetExt],
     sourceExts: [...defaultConfig.resolver.sourceExts, "svg"],
   },
 };
