79151ec96fb453c81b1553ee29c4c46d9206d52e
fcode27 <7506446+fcode27@users.noreply.github.com>
2025-08-13 16:55:23 -0400

    update config.dev.example

--- config.dev.example/index.js
+++ config.dev.example/index.js
@@ -1,5 +1,5 @@
 export const config = {
     API_AUTH_BASE_URL: "http://{LOCAL_URL}",
     COUCHDB_BASE_URL: "http://{LOCAL_URL}",
-    DB_USER_SESSION_KEY: "grmapp-dev-session-key",
+    USER_SESSION_KEY: "grmapp-dev-session-key",
 }
