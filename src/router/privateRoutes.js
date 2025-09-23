6a129c29f93a8b6db2193845f317f43da413626f
fcode27 <7506446+fcode27@users.noreply.github.com>
2025-09-23 11:32:51 -0400

    add db provider

--- src/router/privateRoutes.js
+++ src/router/privateRoutes.js
@@ -1,23 +1,30 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeRouter from "../screens/Home/";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeRouter from '../screens/Home/';
+import { syncServiceInstance } from '../services/shared/SyncService';
+import { DatabaseProvider } from '@nozbe/watermelondb/react';
+
 const Stack = createStackNavigator();
-const PrivateRoutes = () => {
-  return (
-    <Stack.Navigator>
-      {/* //* Home */}
-      <Stack.Screen
-        options={{
-          headerShown: false,
-        }}
-        name="Main"
-        component={HomeRouter}
-      />
-      {/* /* Along with these would come any other route that wouldn't fit inside
+const PrivateRoutes = () =>
+{
+  if (!syncServiceInstance.database) return <></>
+    return (
+      <DatabaseProvider database={syncServiceInstance.database}>
+        <Stack.Navigator>
+          {/* //* Home */}
+          <Stack.Screen
+            options={{
+              headerShown: false,
+            }}
+            name="Main"
+            component={HomeRouter}
+          />
+          {/* /* Along with these would come any other route that wouldn't fit inside
       the bottom tab navigator, meaning any view which doesn't display the tabs
       at the bottom of the screen. */}
-    </Stack.Navigator>
-  );
+        </Stack.Navigator>
+      </DatabaseProvider>
+    );
 };
 
 export default PrivateRoutes;
