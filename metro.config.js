855abd1932af5d997b92980550ba55dbb352be9f
fcode27 <7506446+fcode27@users.noreply.github.com>
2026-05-26 20:50:07 -0400

    docs(api): Add API documentation and remove unused Sentry configuration.

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getDefaultConfig } = require('expo/metro-config')
 
 module.exports = (async () => {
-  const config = await getSentryExpoConfig(__dirname);
+  const config = await getDefaultConfig(__dirname);
   const { transformer, resolver } = config;
 
   config.transformer = {
